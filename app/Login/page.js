ee0ddb09d3e38fd4d378a95502563f8119e404ab
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2024-11-17 16:35:06 +0530

    Login page

---
+++ app/Login/page.js
@@ -0,0 +1,14 @@
+export default function Login(){
+    return(
+        <main>
+            <h1>Login Form</h1>
+            <form>
+                <label>Username:</label>
+                <input type="text"></input><br></br>
+                <label>Password :</label>
+                <input type="password"></input><br></br>
+                <button type="submit">Login</button>
+            </form>
+        </main>
+    )
+}
